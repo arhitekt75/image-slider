33d045838d8349d59dbb9ff51dc8110e6b039fd2
arhitekt75 <arhitekt@blic.net>
2023-01-21 18:07:31 +0100

    drugi komit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// kreiranje hamburger menija
 let openBtn = document.querySelector('#open-btn');
 let closeBtn = document.querySelector('#close-btn');
 let hiddenMenu = document.querySelector('.hidden-menu');
@@ -9,3 +10,26 @@ openBtn.addEventListener('click', ()=>{
 closeBtn.addEventListener('click', ()=>{
     hiddenMenu.style.display='none';
 });
+
+
+// kreiranje img slidera
+let leftBtn = document.querySelector('#left-btn');
+let rightBtn = document.querySelector('#right-btn');
+let pictures = document.querySelectorAll('.img-box img');
+let imgNum = 0;
+
+leftBtn.addEventListener('click', ()=>{
+    console.log('left')
+});
+
+rightBtn.addEventListener('click', ()=>{
+    displayNone();
+});
+
+// sakrivanje svih slika po defaultu
+const displayNone = ()=>{
+    pictures.forEach((img) => {
+        img.style.display='none';
+    }); 
+    
+}
